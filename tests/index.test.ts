24fc4d6cfd26a1aa58bd03a30dccac0cc364fff1
furtiveJack <tanguytraina@hotmail.fr>
2021-01-20 18:55:56 +0100

    Update jest config

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,13 @@
 //FizzBuzz.test.ts
 /// <reference types="jest" />
 
-import {fizzBuzz} from "./index";
+import {fizzBuzz} from "../src/index";
 test("FizzBuzz test", () => {
     expect(fizzBuzz(2)).toBe("1 2 ");
     expect(fizzBuzz(3)).toBe("1 2 Fizz ");
 });
 
-import { isInternalLink } from './index'
+import { isInternalLink } from '../src/index'
 
 test('should return false given external link', () => {
   expect(isInternalLink('https://google.com')).toBe(false)
