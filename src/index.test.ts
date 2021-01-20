c5deda05b8be553524e31896a162f4d42373e43a
ttraina <tanguytraina@hotmail.fr>
2021-01-20 19:48:51 +0100

    Delete index.test.ts

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-//FizzBuzz.test.ts
-/// <reference types="jest" />
-
-import {fizzBuzz} from "./index";
-test("FizzBuzz test", () => {
-    expect(fizzBuzz(2)).toBe("1 2 ");
-    expect(fizzBuzz(3)).toBe("1 2 Fizz ");
-});
