e663f1db64305b0564e5577e26ceaeba2c49d6b9
furtiveJack <tanguytraina@hotmail.fr>
2021-01-20 18:36:11 +0100

    Add other test

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,3 +6,13 @@ test("FizzBuzz test", () => {
     expect(fizzBuzz(2)).toBe("1 2 ");
     expect(fizzBuzz(3)).toBe("1 2 Fizz ");
 });
+
+import { isInternalLink } from './index'
+
+test('should return false given external link', () => {
+  expect(isInternalLink('https://google.com')).toBe(false)
+})
+
+test('should return true given internal link', () => {
+  expect(isInternalLink('/some-page')).toBe(true)
+})
