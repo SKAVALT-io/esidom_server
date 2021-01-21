30039ec032b0a2e80d97f476e20179b9ba8ee485
furtiveJack <tanguytraina@hotmail.fr>
2021-01-21 11:42:41 +0100

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export function fizzBuzz(n: number): string {
     let output = "";
-    for (let i = 0; i < n; i++) {
+    for (let i = 1; i <= n; i++) {
         if (i % 5 && i % 3) {
             output += i + ' ';
         }
